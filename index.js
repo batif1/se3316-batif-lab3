bcf662ffd88a705a86c525325214e62789120592
Bashshar Atif <114458228+batif1@users.noreply.github.com>
2023-11-04 20:06:51 -0400

    frontend is working

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const heros = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 3, name: 'Spiderman' }
+
+
+];
+
+app.use('/', express.static('static'));
+
+app.get('/api/heros', (req, res) => {
+    console.log(`GET request for ${req.url}`)
+    res.send(heros);
 });
 
 app.listen(port, () => {
