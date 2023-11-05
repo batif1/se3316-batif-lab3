9978a9b3bb7b58cb8753de1f6118730164dbb60d
Bashshar Atif <114458228+batif1@users.noreply.github.com>
2023-11-05 11:56:45 -0500

    search by id works

--- index.js
+++ index.js
@@ -1,21 +1,118 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
+const fs = require('fs');
+const { parse } = require('path');
 
-const heros = [
-    { id: 1, name: 'Superman' },
-    { id: 2, name: 'Batman' },
-    { id: 3, name: 'Spiderman' }
+function getHeros() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('superhero_info.json', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    const heros = JSON.parse(data);
+                    resolve(heros);
+                } catch (err) {
+                    reject(err);
+                }
+            }
+        });
+    });
+}
 
 
-];
+// Usage with async/await:
+async function fetchHeros() {
+    try {
+        const heros = await getHeros();
+    } catch (err) {
+        console.error('Error fetching heroes:', err);
+    }
+}
 
+fetchHeros();
+
+
+function getPowers() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('superhero_powers.json', 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    const heros = JSON.parse(data);
+                    resolve(heros);
+                } catch (err) {
+                    reject(err);
+                }
+            }
+        });
+    });
+}
+
+async function fetchPowers() {
+    try {
+        const heros = await getPowers();
+    } catch (err) {
+        console.error('Error fetching Powers:', err);
+    }
+}
+
+fetchPowers();
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(`${req.method} request for ${req.url}`);
+    next();//keep going
+});
+
+//Serves static files
 app.use('/', express.static('static'));
 
-app.get('/api/heros', (req, res) => {
-    console.log(`GET request for ${req.url}`)
-    res.send(heros);
+// Route to get a specific hero by id
+router.get('/:hero_id', async (req,res)=>{
+    try{
+        const heros = await getHeros();
+        const id = parseInt(req.params.hero_id);
+        const hero = heros.find(h => h.id === id);
+        if (hero) {
+            res.send(hero);
+        }else{
+            res.status(404).send('Hero not found');
+        }
+    }catch(err){
+        console.error('Error fetching heroes:', err);
+        res.status(500).send('Server error');
+    }
+
 });
+
+// Route to get all heros
+router.get('/', async (req, res) => {
+    try {
+        const heros = await getHeros();
+        res.send(heros);
+    }catch (err) {
+        console.error('Error fetching heroes:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+
+//To get all powers
+app.get('/api/powers', async (req, res) => {
+    try {
+        const powers = await getPowers();
+        res.send(powers);
+    }catch (err) {
+        console.error('Error fetching powers:', err);
+        res.status(500).send('Server error');
+    }
+});
+
+app.use('/api/heros', router)
 
 app.listen(port, () => {
     console.log(`Listening on port ' ${port})`);
---
+++ static/script.js
@@ -0,0 +1,139 @@
+let globalHeroData = [];
+let globalPowersData = [];
+
+//Displaying heros
+function results(list){
+    list.forEach(hero => {
+        // Create the list item
+        const card = document.createElement('li');
+        card.className = 'card';
+
+        // Create and append the h2 element for name
+        const name = document.createElement('h2');
+        name.textContent = hero.name;
+        card.appendChild(name);
+
+        // Iterate over the hero properties
+        const properties = ['id','Gender', 'Eye color', 'Race', 'Hair color', 'Height', 'Publisher', 'Skin color', 'Alignment', 'Weight'];
+        properties.forEach(prop => {
+          const p = document.createElement('p');
+          // Some properties need to access hero object with bracket notation because of the space in key names
+          p.textContent = `${prop}: ${prop.includes(" ") ? hero[prop] : hero[prop.replace(" ", "")]}`;
+          card.appendChild(p);
+        });
+
+        // Correct the unit for height and weight
+        card.querySelector('p:nth-of-type(6)').textContent += ' cm';
+        card.querySelector('p:last-of-type').textContent += ' lbs';
+
+        // Append the card to the container
+        document.getElementById('superheroCards').appendChild(card);
+      });
+}
+
+
+//Populating a global heros list (AJAX)
+function getHeroList() {
+    return fetch('/api/heros/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            globalHeroData = data; // Store the data globally
+            return data; // Return the data for further processing
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+
+//Populating a power heros list (AJAX)
+function getPowerList() {
+    return fetch('/api/powers')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            globalPowersData = data; // Store the data globally
+            return data; // Return the data for further processing
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+
+
+
+
+
+// for getting the JSON data from the server
+document.getElementById('fetchHerosButton').addEventListener('click', function() {
+    getHeroList().then(data => {
+        const firstTenHeroes = data.slice(0, 10);
+        results(firstTenHeroes);
+    }).catch(error => {
+        console.error('There has been a problem with displaying heroes:', error);
+    });
+});
+
+
+
+ document.getElementById('hero-search').addEventListener('keyup', function() {
+    getHeroList().then(data => {
+        const searchValue = document.getElementById('hero-search').value.trim();
+        document.getElementById('superheroCards').innerHTML = ''; // Clear the previous results
+        // Check if the searchValue is empty or not a number
+        if (searchValue === '' || isNaN(searchValue)) {
+            return; // Early return if searchValue is invalid
+        }
+
+        // Convert searchValue to a number for accurate comparison
+        const searchNumber = Number(searchValue);
+
+        // Filter the globalHeroData array
+        const filteredHeros = globalHeroData.filter(hero => {
+            return hero.id === searchNumber;
+        });
+
+        // Call the results function to display the filtered heroes
+        results(filteredHeros);
+    }).catch(error => {
+        console.error('There has been a problem with displaying heroes:', error);
+    });
+
+    });
+
+
+document.getElementById('power-search').addEventListener('keyup', function() {
+    getPowerList().then(data => {
+        const searchValue = document.getElementById('power-search').value.trim();
+        document.getElementById('superheroCards').innerHTML = ''; // Clear the previous results
+        if (searchValue === '' || isNaN(searchValue)) {
+            return; // Early return if searchValue is invalid
+        }
+        const searchNumber = Number(searchValue);
+        const heroName =  "";
+
+        const targetHero = globalHeroData.filter(hero => {
+            hero.id === searchNumber;
+            heroName = hero.name;
+        });
+
+        const filteredPowers = globalPowersData.filter(power => {
+            return power.name === heroName;
+        });
+
+    }).catch(error => {
+        console.error('There has been a problem with displaying powers:', error);
+    });
+});
+
+
